fcbb44d912aa7f4d7da54c5ef70db76f1c85150e
youssefkhaled16007290 <youssef.khaledahmed@student.giu-uni.de>
2025-11-12 15:17:08 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -4,7 +4,17 @@
 #include "Animal.h"
 
 //define mammal class here
-
+class Enclosure{
+private:
+int capacity;
+int currentCount;
+string []*Animal x;
+public:
+Enclosure(int c,cc);
+void AddAnimal(string a);
+void displayAnimals();
+    
+}
 
 
 
